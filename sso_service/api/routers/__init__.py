adf296e803d1c0410fc3acae5727a6ab218e3a0a
Andre Medvedev <andrejmedvedev2713@gmail.com>
2025-08-23 15:48:36 +0500

    23.08.2025 Раскоментировал Провайдеры

--- sso_service/api/routers/__init__.py
+++ sso_service/api/routers/__init__.py
@@ -5,8 +5,7 @@ from fastapi import APIRouter
 from .admin import admin_router
 from .auth import auth_router
 from .oauth import oauth_router
-
-# from .providers import provider_router  # noqa: ERA001
+from .providers import provider_router
 from .registration import registration_router
 
 router = APIRouter(prefix="/api/v1")
@@ -14,5 +13,5 @@ router = APIRouter(prefix="/api/v1")
 router.include_router(admin_router)
 router.include_router(auth_router)
 router.include_router(oauth_router)
-# router.include_router(provider_router)  # noqa: ERA001
+router.include_router(provider_router)
 router.include_router(registration_router)
