6e0a5d18baf7c683a6fbaccfe6c867290fbdc685
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-08-26 23:30:30 +0500

    26.08.2025 Добавлен email в UserClaims

--- sso_service/core/domain.py
+++ sso_service/core/domain.py
@@ -273,6 +273,7 @@ class ClientClaims(Claims):
 
 
 class UserClaims(Claims):
+    email: EmailStr | None = None
     status: UserStatus | None = None
     realm: str | None = None
     roles: list[Role] | None = None
