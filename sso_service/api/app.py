524d18ce05bf0ab502818ad61d600a6150bc373d
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-08-27 10:40:34 +0500

    27.08.2025 Исправлена обработка ошибок FastAPI

--- sso_service/api/app.py
+++ sso_service/api/app.py
@@ -4,6 +4,7 @@ from contextlib import asynccontextmanager
 
 from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from ..core.exceptions import (
@@ -49,70 +50,71 @@ def setup_errors_handlers(app: FastAPI) -> None:
     def handle_creation_error(
         request: Request,  # noqa: ARG001
         exc: CreationError,
-    ) -> HTTPException:
+    ) -> JSONResponse:
         logger.error(exc)
-        return HTTPException(
+        return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error while resource creation",
+            content={"detail": "Error while resource creation"},
         )
 
     @app.exception_handler(ReadingError)
     def handle_reading_error(
         request: Request,  # noqa: ARG001
         exc: ReadingError,
-    ) -> HTTPException:
+    ) -> JSONResponse:
         logger.error(exc)
-        return HTTPException(
+        return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error while resource reading",
+            content={"detail": "Error while resource reading"},
         )
 
     @app.exception_handler(UpdateError)
     def handle_update_error(
         request: Request,  # noqa: ARG001
         exc: UpdateError,
-    ) -> HTTPException:
+    ) -> JSONResponse:
         logger.error(exc)
-        return HTTPException(
+        return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error while resource update",
+            content={"detail": "Error while resource update"},
         )
 
     @app.exception_handler(AlreadyExistsError)
     def handle_already_exists_error(
         request: Request,  # noqa: ARG001
         exc: AlreadyExistsError,
-    ) -> HTTPException:
+    ) -> JSONResponse:
         logger.error(exc)
-        return HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail="Resource already exists"
+        return JSONResponse(
+            status_code=status.HTTP_409_CONFLICT,
+            content={"detail": "Resource already exists"}
         )
 
     @app.exception_handler(DeletionError)
     def handle_deletion_error(
         request: Request,  # noqa: ARG001
         exc: DeletionError,
-    ) -> HTTPException:
+    ) -> JSONResponse:
         logger.error(exc)
-        return HTTPException(
+        return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error while resource deletion",
+            content={"detail": "Error while resource deletion"}
         )
 
     @app.exception_handler(InvalidCredentialsError)
     def handle_unsupported_grant_type_error(
         request: Request,  # noqa: ARG001
         exc: InvalidCredentialsError,
-    ) -> HTTPException:
-        return HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail=str(exc)
+    ) -> JSONResponse:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST, content={"detail": str(exc)}
         )
 
     @app.exception_handler(UnauthorizedError)
     def handle_unauthorized_error(
         request: Request,  # noqa: ARG001
         exc: UnauthorizedError,
-    ) -> HTTPException:
-        return HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exc)
+    ) -> JSONResponse:
+        return JSONResponse(
+            status_code=status.HTTP_401_UNAUTHORIZED, content={"detail": str(exc)}
         )
