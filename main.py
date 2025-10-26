ebfa34ba22c17450a3d33ecd9009a4fb1bd36af1
andrey baidarka <andrejmedvedev2713@gmail.com>
2025-10-26 15:51:44 +0500

    26.10.2025 Мелкие изменения

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
 
-from sso_service.api.app import create_fastapi_app
+from sso_service.api.app import create_fastapi_app, setup_errors_handlers, setup_middleware
 
 logging.basicConfig(level=logging.INFO)
 
 app = create_fastapi_app()
+setup_middleware(app)
+setup_errors_handlers(app)
